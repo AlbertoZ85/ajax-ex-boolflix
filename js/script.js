2efa53cc0e8a6f9c36df0fa2414a555510185fec
Alberto Zordan <zorberto@gmail.com>
2020-09-08 22:15:04 +0200

    Main tasks done. To do: layout improvement and optional milestones

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo con la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// 1. Titolo
-// 2. Titolo Originale
-// 3. Lingua
-// 4. Voto
-
-// Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
-// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
-
 $(document).ready(function () {
     $('#search-input').focus();
     // Al click del tasto 'search' invoco la funzione 'init'
@@ -57,7 +41,7 @@ function query(input, type) {
                 if (obj.total_results > 0) {
                     listPrint(obj, type)
                 } else {
-                    $(`#${type}-covers`).append('Non ci sono risultati');
+                    $(`#${type}-covers`).css('display', 'block').append(`Non ci sono risultati nella categoria "${type}"`);
                 }
             },
             error: function () {
@@ -83,7 +67,9 @@ function listPrint(obj, type) {
             'flags': flags(thisResult.original_language),
             'language': thisResult.original_language.toUpperCase(),
             'vote': [thisResult.vote_average, stars(thisResult.vote_average)],
-            'type': type == 'movie' ? 'Film' : 'Serie TV'
+            'type': type == 'movie' ? 'Film' : 'Serie TV',
+            'poster': poster(thisResult.poster_path),
+            'overview': thisResult.overview.substring(0, 200) + '[...]'
         };
 
         var html = template(sample);
@@ -143,3 +129,10 @@ function flags(str) {
 Handlebars.registerHelper('isNotString', function (value) {
     return typeof (value) != 'string';
 });
+
+// Gestione della visualizzazione del poster (se presente)
+function poster(data) {
+    var noPosterImg = 'https://i1.wp.com/cinemaome.com/wp-content/uploads/2015/10/cinema-pellicola-film3.jpg?fit=810%2C506';
+    var urlRoot = 'https://image.tmdb.org/t/p/w342';
+    return data == null ? noPosterImg : urlRoot + data;
+}
